d3e1cd1614dbd482bb3452e59081e614acb845f6
孙猛 <sunmengwork@foxmail.com>
2018-12-22 22:22:30 +0800

    feat(container)09

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter',
       home: Scaffold(
-        // appBar: AppBar(
-        //   title:Text('好冷我是哈哈')
-        // ),
+        appBar: AppBar(
+            title: Text(
+          '技术才是程序员唯一的信仰！',
+          style:new TextStyle(
+              color: Color.fromARGB(255, 255, 0, 100), fontFamily: "Oswald"),
+        )),
         body: Center(
-          child: Text(
-            '我很喜欢前端，生存技能，目前属于技能强化阶段，很是开心，因为预计工资会再次的增加哈哈哈',
-            textAlign: TextAlign.start,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 24.0,
-              color: Color.fromARGB(255, 255, 100, 100),
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.double
-            ),
-          ),
-        ),
+            child: Container(
+                child: new Text('技术才是你，唯一的信仰！',
+                    style: TextStyle(
+                      fontSize: 25,
+                    )),
+                alignment: Alignment.topLeft,
+                width: 400.0,
+                height: 300.0,
+                // color: Colors.lightBlueAccent,
+                // padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.fromLTRB(10.0,30.0,40.0,50),
+                margin: const EdgeInsets.all(20),
+                decoration: new BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Colors.redAccent,Colors.purple,Colors.lightBlueAccent]
+                  ),
+                  border: Border.all(width: 5.0,color:Colors.yellow),
+                ), 
+            )),
       ),
     );
   }
