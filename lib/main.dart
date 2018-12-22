42654b2cc3e292c1fcb7c60a06697c39afd736aa
孙猛 <sunmengwork@foxmail.com>
2018-12-22 23:04:27 +0800

    feat(fit)10、fit操作图片，类似background-size

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,29 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
             title: Text(
-          '技术才是程序员唯一的信仰！',
-          style:new TextStyle(
-              color: Color.fromARGB(255, 255, 0, 100), fontFamily: "Oswald"),
-        )),
+            '技术才是程序员唯一的信仰！',
+            style:new TextStyle(
+                color: Colors.white, fontFamily: "Oswald"),
+            )
+        ),
         body: Center(
             child: Container(
-                child: new Text('技术才是你，唯一的信仰！',
-                    style: TextStyle(
-                      fontSize: 25,
-                    )),
-                alignment: Alignment.topLeft,
-                width: 400.0,
-                height: 300.0,
-                // color: Colors.lightBlueAccent,
-                // padding: const EdgeInsets.all(10.0),
-                padding: const EdgeInsets.fromLTRB(10.0,30.0,40.0,50),
-                margin: const EdgeInsets.all(20),
-                decoration: new BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.redAccent,Colors.purple,Colors.lightBlueAccent]
-                  ),
-                  border: Border.all(width: 5.0,color:Colors.yellow),
+                child: new Image.network('https://s1.hdslb.com/bfs/seed/jinkela/header/images/logo_blue.png',
+                    // fit: BoxFit.cover,
+                    // fit: BoxFit.fitWidth,
+                    // fit: BoxFit.contain,
+                    // fit: BoxFit.fill,
+                    color: Colors.red, //背景色
+                    // colorBlendMode: BlendMode.screen, // 混合模式
+                    // repeat: ImageRepeat.repeat,
+                    // repeat: ImageRepeat.repeatX,
+                    repeat: ImageRepeat.repeatY,
                 ), 
-            )),
+                width:300.0,
+                height: 200.0,
+                color: Colors.purple[50],
+            ),
+        ),
       ),
     );
   }
